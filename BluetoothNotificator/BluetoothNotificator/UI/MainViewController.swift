4779d2a9fc2d7213140a0d7fdeed053718d3dc0b
Grzegorz Gumieniak <grzegorzgumieniak@Grzegorzs-MacBook-Pro.local>
2022-03-09 20:37:42 +0100

    Remove storyboards

---
+++ BluetoothNotificator/BluetoothNotificator/UI/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  BluetoothNotificator
+//
+//  Created by Grzegorz Gumieniak on 09/03/2022.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+	override func loadView() {
+		super.loadView()
+		
+		view.backgroundColor = .red
+	}
+}
